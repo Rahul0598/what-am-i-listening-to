6587ce56bd98dd7f9b05dfb895f7b52fb4a4e512
Devajji <devajjir@gmail.com>
2020-12-23 11:39:18 +0530

    display podcast publisher name

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -1,28 +1,34 @@
 const SongCard = track => {
+    console.log();
     if(track.track.uri.includes("track")){
         return (
-            <iframe
-                title="Spotify"
-                className="SpotifyPlayer"
-                src={`https://embed.spotify.com/?uri=${track.track.uri}&view=coverart`}
-                width={"100%"}
-                height={380}
-                frameBorder="0"
-                allowtransparency="true"
-            />
+            <div>
+                <iframe
+                    title="Spotify"
+                    className="SpotifyPlayer"
+                    src={`https://embed.spotify.com/?uri=${track.track.uri}&view=coverart`}
+                    width={300}
+                    height={380}
+                    frameBorder="0"
+                    allowtransparency="true"
+                />
+            </div>
         )
     }
     else {
         return (
-            <iframe
-                title="Spotify"
-                className="SpotifyPlayer"
-                src={`https://open.spotify.com/embed-podcast/episode/${track.track.id}`}
-                width={"100%"}
-                height={380}
-                frameBorder="0"
-                allowtransparency="true"
-            />
+            <div>
+                <p>{track.track.show.publisher}</p>
+                <iframe
+                    title="Spotify"
+                    className="SpotifyPlayer"
+                    src={`https://open.spotify.com/embed-podcast/episode/${track.track.id}`}
+                    width={"100%"}
+                    height={380}
+                    frameBorder="0"
+                    allowtransparency="true"
+                />
+            </div>
         )
     }
 }
